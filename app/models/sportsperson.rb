157ca2e3950e180f0c437031a21506cc2ea9f60a
MC <chokamoses2@gmail.com>
2024-03-04 11:41:50 +0300

    added user show page with image attached

--- app/models/sportsperson.rb
+++ app/models/sportsperson.rb
@@ -1,3 +1,12 @@
 class Sportsperson < ApplicationRecord
   has_one_attached :image
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["birthdate", "created_at", "email", "firstname", "id", "lastname", "phone", "position", "team", "updated_at"]
+
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["image_attachment", "image_blob"]
+  end
 end
